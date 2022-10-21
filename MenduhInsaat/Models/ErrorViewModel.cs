250bb9fad81cefd98d3b72ce307c62e7c8c4531d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-21 20:36:37 +0300

    Add project files.

---
+++ MenduhInsaat/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MenduhInsaat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
