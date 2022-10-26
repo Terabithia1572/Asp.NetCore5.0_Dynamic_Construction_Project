edaf8e97a6ccb61d222f80e16f5c81d5d00956c0
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-26 22:58:51 +0300

    Special Product Create+Update+Delete Process / Added

--- MenduhInsaat/Controllers/SpecialProductController.cs
+++ MenduhInsaat/Controllers/SpecialProductController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -19,6 +20,55 @@ namespace MenduhInsaat.Controllers
         {
             var values = specialProductManager.GetList();
             return View(values);
+        }
+
+        public IActionResult SpecialProductList()
+        {
+            var values = specialProductManager.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddSpecialProduct()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSpecialProduct(SpecialProduct specialProduct)
+        {
+            Random r = new Random();
+           int sayi= r.Next(8, 100);
+            specialProduct.SpecialProductTabIndex = "tab-"+sayi;
+            specialProductManager.TAdd(specialProduct);
+            return RedirectToAction("SpecialProductList", "SpecialProduct");
+        }
+
+        public IActionResult DeleteSpecialProduct(int id)
+        {
+            var productvalue = specialProductManager.TGetByID(id);
+            specialProductManager.TDelete(productvalue);
+            return RedirectToAction("SpecialProductList", "SpecialProduct");
+        }
+        [HttpGet]
+        public IActionResult EditSpecialProduct(int id)
+        {
+           
+            var productvalues = specialProductManager.TGetByID(id);
+
+            return View(productvalues);
+        }
+        [HttpPost]
+
+        public IActionResult EditSpecialProduct(SpecialProduct specialProduct)
+        {
+
+
+            Random r = new Random();
+            int sayi = r.Next(8, 100);
+            specialProduct.SpecialProductTabIndex = "tab-" + sayi;
+            
+            specialProductManager.TUpdate(specialProduct);
+            return RedirectToAction("SpecialProductList", "SpecialProduct");
         }
     }
 }
