30cff628ce75b02d6902d9723e774cc7f2fac10c
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-27 19:34:35 +0300

    Image Controller + Image List + Image Page / Added

---
+++ MenduhInsaat/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenduhInsaat.Controllers
+{
+    public class ImageController : Controller
+    {
+        ImageManager imageManager = new ImageManager(new EfImageRepository());
+        Context context = new Context();
+
+        public IActionResult Index()
+        {
+            var values = imageManager.GetList();
+            return View(values);
+        }
+    }
+}
---
+++ MenduhInsaat/ViewComponents/Images.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenduhInsaat.ViewComponents
+{
+    public class Images:ViewComponent
+    {
+        ImageManager imageManager = new ImageManager(new EfImageRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = imageManager.GetList();
+            return View(values);
+        }
+    }
+}
