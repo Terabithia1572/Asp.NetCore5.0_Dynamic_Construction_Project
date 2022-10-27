b68506bbddd237b973b4e8f9b26e14e737bda81a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-10-28 01:04:42 +0300

    Employee List + Add+Delete / Added

--- DataAccessLayer/Models/DTOs/EmployeeImageAdd.cs
+++ DataAccessLayer/Models/DTOs/EmployeeImageAdd.cs
@@ -1,25 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer.Concrete
+namespace DataAccessLayer.Models.DTOs
 {
-    public class Employee
+    public class EmployeeImageAdd
     {
-        [Key]
-        public int EmployeeID { get; set; }
         public string EmployeeName { get; set; }
         public string EmployeeSurName { get; set; }
-        public string EmployeeImage { get; set; }
+        public IFormFile EmployeeImage { get; set; }
         public string EmployeeTwitter { get; set; }
         public string EmployeeFacebook { get; set; }
         public string EmployeeInstagram { get; set; }
         public string EmployeeLinkedin { get; set; }
         public bool EmployeeStatus { get; set; }
-
-
+        
     }
 }
--- MenduhInsaat/Controllers/EmployeeController.cs
+++ MenduhInsaat/Controllers/EmployeeController.cs
@@ -1,9 +1,12 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Models.DTOs;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,5 +28,46 @@ namespace MenduhInsaat.Controllers
             var values = employeeManager.GetList();
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeImageAdd employeeImageAdd)
+        {
+            Employee employee = new Employee();
+            if (employeeImageAdd.EmployeeImage != null)
+            {
+                var extension = Path.GetExtension(employeeImageAdd.EmployeeImage.FileName);
+                var newimageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/EmployeeImageFolder/", newimageName);
+                var stream = new FileStream(location, FileMode.Create);
+                employeeImageAdd.EmployeeImage.CopyTo(stream);
+                employee.EmployeeImage = "/EmployeeImageFolder/" + newimageName;
+            }
+            employee.EmployeeName = employeeImageAdd.EmployeeName;
+            employee.EmployeeSurName = employeeImageAdd.EmployeeSurName;
+            employee.EmployeeTwitter = employeeImageAdd.EmployeeTwitter;
+            employee.EmployeeFacebook = employeeImageAdd.EmployeeFacebook;
+            employee.EmployeeInstagram = employeeImageAdd.EmployeeInstagram;
+            employee.EmployeeLinkedin = employeeImageAdd.EmployeeLinkedin;
+            employee.EmployeeStatus = true;
+            employeeManager.TAdd(employee);
+            return RedirectToAction("EmployeeList", "Employee");
+        }
+
+        public IActionResult DeleteEmployee(int id)
+        {
+            var values = employeeManager.TGetByID(id);
+
+            employeeManager.TDelete(values);
+
+            return RedirectToAction("EmployeeList","Employee");
+        }
+
+        
     }
 }
