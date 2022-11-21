d41afdfa6c07633f47b2c162382f3d8ee86a5d52
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-11-21 22:48:25 +0300

    Image Page updated + Paged List / Added

--- MenduhInsaat/ViewComponents/Images.cs
+++ MenduhInsaat/ViewComponents/Images.cs
@@ -5,15 +5,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace MenduhInsaat.ViewComponents
 {
     public class Images:ViewComponent
     {
         ImageManager imageManager = new ImageManager(new EfImageRepository());
-        public IViewComponentResult Invoke()
+        public async Task<IViewComponentResult> InvokeAsync(int page=1)
         {
-            var values = imageManager.GetList();
+            var images = imageManager.GetList();
+            var values =await images.ToPagedListAsync(page, 4);
             return View(values);
         }
 
