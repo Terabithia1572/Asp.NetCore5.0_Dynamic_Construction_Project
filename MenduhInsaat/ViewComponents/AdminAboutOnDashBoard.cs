6724d60fd14f5826eb8774417666a0dca279bc9e
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2022-11-28 00:59:33 +0300

    Retrieve Information of User Logged in by ID / Added

--- MenduhInsaat/ViewComponents/AdminAboutOnDashBoard.cs
+++ MenduhInsaat/ViewComponents/AdminAboutOnDashBoard.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,9 +15,24 @@ namespace MenduhInsaat.ViewComponents
     {
         Context context = new Context();
         AdminManager adminManager = new AdminManager(new EfAdminRepository());
-        public IViewComponentResult Invoke()
+        private readonly UserManager<AppUser> _userManager;
+
+        public AdminAboutOnDashBoard(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
         {
             //var userName = context.Admins.Where(x => x.Username == "Admin").Select(y => y.Name).FirstOrDefault();
+            //var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            //var usermail = User.Identity.Name;
+            //var username = context.Admins.Where(x => x.Username == usermail).Select(y => y.Name).FirstOrDefault();
+            //ViewBag.v1 = username;
+            var username = User.Identity.Name;
+            ViewBag.v1 = username;
+            var usermail = context.Admins.Where(x => x.Username == username).Select(y => y.Name).FirstOrDefault();
+            var adminID = context.Admins.Where(x => x.Name == usermail).Select(y => y.AdminID).FirstOrDefault();
             var values = adminManager.GetList();
             return View(values);
         }
